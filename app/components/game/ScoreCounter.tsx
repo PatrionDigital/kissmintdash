e95f2889552e25cf8a151d26262871a27b9cfe8d
patrion <patrick.s.davis@gmail.com>
2025-05-21 23:31:59 +0900

    chore: update ts-ignore comment to ts-expect-error in ScoreCounter component

--- app/components/game/ScoreCounter.tsx
+++ app/components/game/ScoreCounter.tsx
@@ -8,7 +8,7 @@ export interface ScoreCounterProps {
 
 export const ScoreCounter: React.FC<ScoreCounterProps> = ({ score }) => (
   <div className="flex items-center justify-center gap-3 text-4xl font-extrabold text-center text-accent drop-shadow">
-    {/* @ts-ignore */}
+    {/* @ts-expect-error */}
     <Display value={score.toString().padStart(3, '0')} color="lime" height={64} count={3} backgroundColor="transparent" />
   </div>
 );
