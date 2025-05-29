ed9e10674f7cef9d133f581055e7be57dfd6a72a
patrion <patrick.s.davis@gmail.com>
2025-05-29 16:43:44 +0900

    style: update home button with blue color scheme and custom home icon

--- app/components/SystemMenu.tsx
+++ app/components/SystemMenu.tsx
@@ -411,8 +411,12 @@ const SystemMenu: React.FC<SystemMenuProps> = ({ setActiveTab }) => {
           <Button
             variant="ghost"
             size="lg"
-            className="w-full justify-start text-red-500 hover:text-red-600 dark:text-red-400 dark:hover:text-red-300 mt-4"
-            icon="x-circle"
+            className="w-full justify-start text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 mt-4"
+            icon={
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+              </svg>
+            }
             onClick={() => setActiveTab('home')}
           >
             Back to Home
