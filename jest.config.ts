2120781f8e383c8fa61a5e7455298b2f7c0baf39
patrion <patrick.s.davis@gmail.com>
2025-06-08 21:33:40 +0900

    style: convert jest config quotes from single to double quotes

--- jest.config.ts
+++ jest.config.ts
@@ -1,42 +1,42 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/app/$1',
-    '^@/app/(.*)$': '<rootDir>/app/$1',
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/styles/(.*)$': '<rootDir>/styles/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
+    "^@/(.*)$": "<rootDir>/app/$1",
+    "^@/app/(.*)$": "<rootDir>/app/$1",
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/styles/(.*)$": "<rootDir>/styles/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
-  setupFiles: ['dotenv/config', './tests/setup.ts'],
+  setupFiles: ["dotenv/config", "./tests/setup.ts"],
   testTimeout: 30000, // 30 seconds timeout for async tests
   transform: {
-    '^.+\\.(t|j)sx?$': [
-      'ts-jest',
+    "^.+\\.(t|j)sx?$": [
+      "ts-jest",
       {
         useESM: true,
         isolatedModules: true,
-        tsconfig: 'tsconfig.json',
+        tsconfig: "tsconfig.json",
       },
     ],
   },
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleDirectories: ['node_modules', '<rootDir>'],
-  moduleFileExtensions: ['js', 'json', 'ts'],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleFileExtensions: ["js", "json", "ts"],
   transformIgnorePatterns: [
-    'node_modules/(?!(jose|@coinbase/cdp-sdk|uuid|@coinbase/cdp-sdk/node-fetch|node-fetch|@coinbase/cdp-sdk/node_modules/jose)/)',
+    "node_modules/(?!(jose|@coinbase/cdp-sdk|uuid|@coinbase/cdp-sdk/node-fetch|node-fetch|@coinbase/cdp-sdk/node_modules/jose)/)",
   ],
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       useESM: true,
       isolatedModules: true,
-      tsconfig: 'tsconfig.json',
+      tsconfig: "tsconfig.json",
     },
   },
-  resolver: 'jest-ts-webcompat-resolver',
+  resolver: "jest-ts-webcompat-resolver",
 };
 
 export default config;
